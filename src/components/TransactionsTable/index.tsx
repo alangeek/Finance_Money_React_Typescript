b6294badc9172f57d6781e9144eee3bc0f46d403
Alan Christian <alanchristiancreator@hotmail.com>
2021-03-26 19:39:47 -0300

    Formatting values

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -8,7 +8,7 @@ interface Transation {
   amount: number;
   type: string;
   category: string;
-  createAt: string;
+  createdAt: string;
 }
 
 export function TransactionsTable() {
@@ -35,9 +35,18 @@ export function TransactionsTable() {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createAt}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
